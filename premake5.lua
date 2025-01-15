88e140a6bdb06494e6b5a6939d853b28927b107a
davemccann <davemccann@users.noreply.github.com>
2025-01-15 00:12:04 +0000

    Updated premake5.lua
    ConfigOutputPath is now set in the library rather than controlled externally, defaults to cfg.platform and cfg.buildcfg values

--- premake5.lua
+++ premake5.lua
@@ -1,45 +1,46 @@
+local configOutputPath = "%{cfg.platform}/%{cfg.buildcfg}"
 
-project "ImGui"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "Off"
+project("ImGui")
+kind("StaticLib")
+language("C++")
+cppdialect("C++17")
+staticruntime("Off")
 
-    targetdir ("bin/" .. ConfigOutputPath)
-    objdir ("bin-obj/" .. ConfigOutputPath)
+targetdir("bin/" .. configOutputPath)
+objdir("bin-obj/" .. configOutputPath)
 
-    configmap {
-      ["Debug"] = "DebugStatic",
-      ["Release"] = "ReleaseStatic"
-    }
+configmap({
+    ["Debug"] = "DebugStatic",
+    ["Release"] = "ReleaseStatic",
+})
 
-    includedirs {
-        "$(ProjectDir)"
-    }
+includedirs({
+    "%{prj.location}",
+})
 
-    files {
-        "misc/cpp/imgui_stdlib.h",
-        "misc/cpp/imgui_stdlib.cpp",
-        "imconfig.h",
-        "imgui.h",
-        "imgui.cpp",
-        "imgui_demo.cpp",
-        "imgui_draw.cpp",
-        "imgui_internal.h",
-        "imgui_tables.cpp",
-        "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h"
-    }
+files({
+    "misc/cpp/imgui_stdlib.h",
+    "misc/cpp/imgui_stdlib.cpp",
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_demo.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_tables.cpp",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+})
 
-    filter "platforms:x64"
-        system "Windows"
+filter("platforms:x64")
+system("Windows")
 
-    filter "configurations:Debug*"
-        runtime "Debug"
-        symbols "On"
+filter("configurations:Debug*")
+runtime("Debug")
+symbols("On")
 
-    filter "configurations:Release*"
-        runtime "Release"
-        optimize "Speed"
+filter("configurations:Release*")
+runtime("Release")
+optimize("Speed")
